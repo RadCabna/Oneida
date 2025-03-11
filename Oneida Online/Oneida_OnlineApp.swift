7eec21d96569e0a5572eda2830c46a58c0eb6783
Radcabna <radcabna@gmail.com>
2025-03-11 16:16:04 +0300

    Initial Commit

---
+++ Oneida Online/Oneida_OnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oneida_OnlineApp.swift
+//  Oneida Online
+//
+//  Created by Алкександр Степанов on 11.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Oneida_OnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
